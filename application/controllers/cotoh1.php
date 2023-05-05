100c93b9d2a690aca59b4aeea84a89531ce6c0c4
wisnuw23 <wisnuwicaksono20@gmail.com>
2023-05-05 20:51:59 +0700

    first commit

---
+++ application/controllers/cotoh1.php
@@ -0,0 +1,12 @@
+<?php
+class cotoh1 extends CI_Controller
+{
+   public function index()
+   {
+     echo "<h1>perkenalkan </h1>";
+     echo "nama saya wisnu wicaksono
+        saya tinggal di daerah jakarta barat 
+        olah raga yang saya suka adalah 
+        futsal";
+   }    
+}  
---
+++ application/views/v_footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <a href="http://www.RentalBuku.com">RentalBuku2023.WisnuWicaksono</a>
+</footer>
+</div>
+</body>
